4152a49e53089099cd4a5cf652401cd2f9f1ca6a
rmichela <deltahat@gmail.com>
2012-06-25 15:49:43 -0400

    Perfected death mechanic

--- src/main/java/com/ryanmichela/toxicskies/DamageApplyTask.java
+++ src/main/java/com/ryanmichela/toxicskies/DamageApplyTask.java
@@ -14,7 +14,11 @@ public class DamageApplyTask implements Runnable{
     @Override
     public void run() {
         player.sendMessage(TsSettings.getAboveGroundMessage());
-        player.damage(TsSettings.getAboveGroundDamage());
+        if (player.getHealth() <= 10) {
+            player.damage(TsSettings.getAboveGroundDamage());
+        }
+        player.setFoodLevel(0);
+        player.setSaturation(0);
         player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, TsSettings.getAboveGroundEffectDuration(), 0));
         player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, TsSettings.getAboveGroundEffectDuration(), 0));
         player.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, TsSettings.getAboveGroundEffectDuration(), 0));
